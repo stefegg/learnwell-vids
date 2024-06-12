51f2e76eeca6b32e16dd901452e70925b94d4db4
stefegg <stephenegbert@gmail.com>
2024-06-12 18:44:15 -0400

    remove unused image

--- public/static/images/index.tsx
+++ public/static/images/index.tsx
@@ -3,8 +3,7 @@ import books from "./books.png";
 import hiero from "./hiero.jpeg";
 import jungle from "./jungle.jpg";
 import pharoh from "./pharoh.png";
-import pyramid from "./pyramid.png";
 import stars from "./stars.jpeg";
 import trex from "./trex.png";
 
-export { astronaut, books, hiero, jungle, pharoh, pyramid, stars, trex };
+export { astronaut, books, hiero, jungle, pharoh, stars, trex };
